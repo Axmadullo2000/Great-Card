f05cba0f70b46a449a5ebe5f74d70589fb027242
Axmadullo <axmadullo2000@gmail.com>
2021-11-21 17:55:57 +0500

    Changes in this time!

--- great_card/urls.py
+++ great_card/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from store.views import store
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product.urls')),
-    path('store/', include('store.urls'))
+    path('store/', include('store.urls')),
+    path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
